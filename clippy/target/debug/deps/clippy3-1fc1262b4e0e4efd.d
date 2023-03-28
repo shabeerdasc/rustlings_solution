13b820f7993e57783e7157325ff2d96d558934e4
shabeerdas <shabeerdasc@gmail>
2023-03-28 15:42:48 +0530

    completed solution

---
+++ clippy/target/debug/deps/clippy3-1fc1262b4e0e4efd.d
@@ -0,0 +1,8 @@
+/home/shabeerdas/rust/rustlings/exercises/clippy/target/debug/deps/clippy3-1fc1262b4e0e4efd.rmeta: clippy3.rs Cargo.toml
+
+/home/shabeerdas/rust/rustlings/exercises/clippy/target/debug/deps/clippy3-1fc1262b4e0e4efd.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
